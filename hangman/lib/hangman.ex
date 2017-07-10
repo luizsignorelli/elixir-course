c441cef39f7277f93a0ca188c205bd52f404912c
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2017-07-09 23:44:33 -0300

    fix api

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,9 +2,10 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(),  to: Game
-  defdelegate make_move(game, guess), to: Game
+  defdelegate tally(game), to: Game
 
-  def tally(game) do
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
     { game, tally(game) }
   end
 end
